4766dc541d0a3dec72f26590ab66a5fd88dbfe79
MPrecelle <tinchoprecelle@gmail.com>
2022-02-01 15:26:06 -0300

    Create model Restaurant

--- src/models/Restaurant.js
+++ src/models/Restaurant.js
@@ -1,1 +1,47 @@
-var restaurant = 'restaurant';
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+    sequelize.define('restaurant', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        neighborhood_info: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        cuisine: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            allowNull: false
+        },
+        photo: {
+            type: DataTypes.STRING,
+        },
+        personas_max: {
+            type: DataTypes.NUMBER,
+            allowNull: false
+        }
+    }, {
+        createdAt: false,
+        updatedAt: false,
+    })
+}
