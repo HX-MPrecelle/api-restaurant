d26c0479c96ad844c78362fc7062dfff5c28ee07
MPrecelle <tinchoprecelle@gmail.com>
2022-02-04 15:33:21 -0300

    Add middleware index.js and create neighborhood route get

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const restaurantRoute = require('./restaurantRoute');
+const neighborhoodRoute = require('./neighborhoodRoute');
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
@@ -8,5 +9,6 @@ const router = Router();
 // Configurar los routers
 // // Ejemplo: router.use('/auth', authRouter);
 router.use('/restaurant', restaurantRoute)
+router.use('/neighborhood', neighborhoodRoute)
 
 module.exports = router;
