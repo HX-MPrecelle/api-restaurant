debc8a9683360cd5b71b30c164c2d1235b5cf783
MPrecelle <tinchoprecelle@gmail.com>
2022-02-04 14:28:04 -0300

    Create restaurant/create post route

--- src/routes/restaurantRoute.js
+++ src/routes/restaurantRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { getAllRestaurants } = require('../controller/controller');
+const { Restaurant, Type } = require('../db');
 
 const router = express.Router();
 
@@ -26,6 +27,44 @@ router.get('/:id', async (req, res) => {
         if (id) {
             let restaurant = allRestaurants.filter(e => e.id == id);
             restaurant.length ? res.status(200).send(restaurant) : res.status(404).send('Restaurant not found');
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+router.post('/create', async (req, res) => {
+    const {name, address, neighborhood_info, cuisine, email, personas_max, photo} = req.body;
+    try {
+        if (name && email) {
+            const allRestaurants = await getAllRestaurants();
+            const restaurantName = allRestaurants.filter(e => e.name?.toLowerCase() === name?.toLowerCase());
+            const restaurantEmail = allRestaurants.filter(e => e.email?.toLowerCase() === email?.toLowerCase());
+            if (!restaurantName.length && !restaurantEmail.length) {
+                const restaurant = await Restaurant.create({
+                    name,
+                    address,
+                    neighborhood_info,
+                    cuisine,
+                    email,
+                    personas_max, 
+                    photo
+                });
+
+                const cuisinesType = await Type.findAll({
+                    where: {
+                        name: cuisine,
+                    }
+                });
+
+                restaurant.addType(cuisinesType);
+                return res.status(201).send(restaurant);
+            } else {
+                return res.status(406).send('Restaurant name or email already exist');
+            }
+        }
+        if (!name || !address || !neighborhood_info || !cuisine || !email || !personas_max) {
+            return res.status(400).send('Data incomplete')
         }
     } catch (e) {
         console.log(e);
