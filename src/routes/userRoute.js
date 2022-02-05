05361c2354d9ffed93d9bc46a1b35872bea71b88
MPrecelle <tinchoprecelle@gmail.com>
2022-02-05 14:38:36 -0300

    Validations and encrypt password on register

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,27 +1,45 @@
 const express = require('express');
 const { User } = require('../db');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
 
 router.post('/create', async (req, res) => {
-    const {username, email, password} = req.body;
+    const {username, email, password, password2} = req.body;
+    
+    let errors = [];
+    
+    if (!username || !email || !password || !password2) {
+        errors.push({message: 'Please enter all fields'})
+    }
+
+    if (password.length < 6) {
+        errors.push({message: 'Password should be at least 6 characters'})
+    }
+
+    if (password != password2) {
+        errors.push({message: 'Passwords do not match'})
+    }
+
+    if (errors.length) {
+        return res.status(400).send(errors)
+    }
+    
     try {
-        if (username && email && password) {
-            const allUser = await User.findAll();
-            const userUsername = allUser.find(e => e.username.toLowerCase() === username.toLowerCase());
-            const userEmail = allUser.find(e => e.email.toLowerCase() === email.toLowerCase());
-            if (userUsername) return res.status(400).send('Username already exist');
-            if (userEmail) return res.status(400).send('Email already exist');
-            const newUser = await User.create({
-                username,
-                email,
-                password
-            });
-            return res.status(200).send(newUser);
-        } else {
-            return res.status(400).send('Data incomplete');
-        }
+        const allUser = await User.findAll();
+        const userUsername = allUser.find(e => e.username.toLowerCase() === username.toLowerCase());
+        const userEmail = allUser.find(e => e.email.toLowerCase() === email.toLowerCase());
+        if (userUsername) return res.status(400).send('Username already exist');
+        if (userEmail) return res.status(400).send('Email already exist');
+        //Hasheo la password para enviarla de forma segura a la DB, el segundo parametro son las vueltas de encriptación que queremos darle.
+        let hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+        return res.status(200).send(newUser); 
     } catch (e) {
         console.log(e);
     }
