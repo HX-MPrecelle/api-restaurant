76a8864e7b3f54b998420effcb15a2248065433c
MPrecelle <tinchoprecelle@gmail.com>
2022-02-07 20:45:12 -0300

    Create login route post google

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -9,13 +9,13 @@ router.post('/',  async (req, res) => {
     const {email, password} = req.body;
     
     if (!email || !password) {
-        return res.status(400).send({message: 'Please enter all fields'})
+        return res.status(400).send({message: 'Por favor, ingrese complete los campos'})
     }
     
     try {
         const allUser = await User.findAll();
         const user = allUser.find(e => e.email.toLowerCase() === email.toLowerCase());
-        console.log(user);
+        // console.log(user);
         if (user) {
             let passHash = bcrypt.compareSync(password, user.password);
             if (passHash) {
@@ -26,16 +26,38 @@ router.post('/',  async (req, res) => {
                 }
                 return res.status(200).send(loggedUser);   
             } else {
-                return res.status(400).json({message: 'Passwords not match'});
+                return res.status(400).json({message: 'Las contraseñas no coinciden'});
             }
         } else {
-            return res.status(404).json({message: 'Email is not valid'});
+            return res.status(404).json({message: 'El usuario no existe'});
         }
     } catch (e) {
         console.log(e);
-        return res.status(404).json({message: 'Not found'});
+        return res.status(404).json({message: 'Respuesta inválida'});
     }
 });
+
+router.post('/google', async (req, res) => {
+    const { email, id } = req.body;
+
+    if (!email || !id) {
+        return res.status(400).send({message: 'Por favor, ingresa nuevamente'})
+    }
+
+    try {
+        const allUser = await User.findAll();
+        const user = allUser.find(e => e.email.toLowerCase() === email.toLowerCase());
+        // console.log(user);
+        if (user) {
+            return res.status(200).json({id: user.id, email: user.email, username: user.username});   
+        } else {
+            return res.status(404).json({message: 'Usuario no encontrado'});
+        }
+    } catch (e) {
+        // console.log(e);
+        return res.status(404).json({message: 'Petición inválida'});
+    }
+})
 
 module.exports = router;
 
