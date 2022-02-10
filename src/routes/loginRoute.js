977f5332852788e726688952da7c5d75e58869a7
MPrecelle <tinchoprecelle@gmail.com>
2022-02-10 08:19:27 -0300

    Format Document

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -1,63 +1,72 @@
 //verificar que exista usuario sino mandar a registro y unicamente mandar respuesta al front con error o success tanto para login normal como para google
-const express = require('express');
-const { User } = require('../db');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const { User } = require("../db");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
-router.post('/',  async (req, res) => {
-    const {email, password} = req.body;
-    
-    if (!email || !password) {
-        return res.status(400).send({message: 'Por favor, ingrese complete los campos'})
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .send({ message: "Por favor, ingrese complete los campos" });
+  }
+
+  try {
+    const allUser = await User.findAll();
+    const user = allUser.find(
+      (e) => e.email.toLowerCase() === email.toLowerCase()
+    );
+    // console.log(user);
+    if (user) {
+      let passHash = bcrypt.compareSync(password, user.password);
+      if (passHash) {
+        var loggedUser = {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+        };
+        return res.status(200).send(loggedUser);
+      } else {
+        return res
+          .status(400)
+          .json({ message: "Las contraseñas no coinciden" });
+      }
+    } else {
+      return res.status(404).json({ message: "El usuario no existe" });
     }
-    
-    try {
-        const allUser = await User.findAll();
-        const user = allUser.find(e => e.email.toLowerCase() === email.toLowerCase());
-        // console.log(user);
-        if (user) {
-            let passHash = bcrypt.compareSync(password, user.password);
-            if (passHash) {
-                var loggedUser = {
-                    id: user.id,
-                    username: user.username,
-                    email: user.email
-                }
-                return res.status(200).send(loggedUser);   
-            } else {
-                return res.status(400).json({message: 'Las contraseñas no coinciden'});
-            }
-        } else {
-            return res.status(404).json({message: 'El usuario no existe'});
-        }
-    } catch (e) {
-        console.log(e);
-        return res.status(404).json({message: 'Respuesta inválida'});
-    }
+  } catch (e) {
+    console.log(e);
+    return res.status(404).json({ message: "Respuesta inválida" });
+  }
 });
 
-router.post('/google', async (req, res) => {
-    const { email, id } = req.body;
+router.post("/google", async (req, res) => {
+  const { email, id } = req.body;
 
-    if (!email || !id) {
-        return res.status(400).send({message: 'Por favor, ingresa nuevamente'})
-    }
+  if (!email || !id) {
+    return res.status(400).send({ message: "Por favor, ingresa nuevamente" });
+  }
 
-    try {
-        const allUser = await User.findAll();
-        const user = allUser.find(e => e.email.toLowerCase() === email.toLowerCase());
-        // console.log(user);
-        if (user) {
-            return res.status(200).json({id: user.id, email: user.email, username: user.username});   
-        } else {
-            return res.status(404).json({message: 'Usuario no encontrado'});
-        }
-    } catch (e) {
-        // console.log(e);
-        return res.status(404).json({message: 'Petición inválida'});
+  try {
+    const allUser = await User.findAll();
+    const user = allUser.find(
+      (e) => e.email.toLowerCase() === email.toLowerCase()
+    );
+    // console.log(user);
+    if (user) {
+      return res
+        .status(200)
+        .json({ id: user.id, email: user.email, username: user.username });
+    } else {
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
-})
+  } catch (e) {
+    // console.log(e);
+    return res.status(404).json({ message: "Petición inválida" });
+  }
+});
 
 module.exports = router;
-
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,48 +1,51 @@
-const express = require('express');
-const { User } = require('../db');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const { User } = require("../db");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
+router.post("/create", async (req, res) => {
+  const { username, email, password, password2 } = req.body;
 
-router.post('/create', async (req, res) => {
-    const {username, email, password, password2} = req.body;
-    
-    let errors = [];
-    
-    if (!username || !email || !password || !password2) {
-        errors.push({message: 'Please enter all fields'})
-    }
+  let errors = [];
 
-    if (password.length < 6) {
-        errors.push({message: 'Password should be at least 6 characters'})
-    }
+  if (!username || !email || !password || !password2) {
+    errors.push({ message: "Please enter all fields" });
+  }
 
-    if (password != password2) {
-        errors.push({message: 'Passwords do not match'})
-    }
+  if (password.length < 6) {
+    errors.push({ message: "Password should be at least 6 characters" });
+  }
 
-    if (errors.length) {
-        return res.status(400).send(errors)
-    }
-    
-    try {
-        const allUser = await User.findAll();
-        const userUsername = allUser.find(e => e.username.toLowerCase() === username.toLowerCase());
-        const userEmail = allUser.find(e => e.email.toLowerCase() === email.toLowerCase());
-        if (userUsername) return res.status(400).send('Username already exist');
-        if (userEmail) return res.status(400).send('Email already exist');
-        //Hasheo la password para enviarla de forma segura a la DB, el segundo parametro son las vueltas de encriptación que queremos darle.
-        let hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await User.create({
-            username,
-            email,
-            password: hashedPassword
-        });
-        return res.status(200).send(newUser); 
-    } catch (e) {
-        console.log(e);
-    }
+  if (password != password2) {
+    errors.push({ message: "Passwords do not match" });
+  }
+
+  if (errors.length) {
+    return res.status(400).send(errors);
+  }
+
+  try {
+    const allUser = await User.findAll();
+    const userUsername = allUser.find(
+      (e) => e.username.toLowerCase() === username.toLowerCase()
+    );
+    const userEmail = allUser.find(
+      (e) => e.email.toLowerCase() === email.toLowerCase()
+    );
+    if (userUsername) return res.status(400).send("Username already exist");
+    if (userEmail) return res.status(400).send("Email already exist");
+    //Hasheo la password para enviarla de forma segura a la DB, el segundo parametro son las vueltas de encriptación que queremos darle.
+    let hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    return res.status(200).send(newUser);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
