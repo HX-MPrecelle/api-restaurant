c49bdcbcc87c93b365f1dedcd9a96f60e9fc5cb1
MPrecelle <tinchoprecelle@gmail.com>
2022-02-10 19:13:39 -0300

    Fix reserve post route and create reserve delete route

--- src/routes/reservesRoute.js
+++ src/routes/reservesRoute.js
@@ -24,25 +24,85 @@ router.post("/", async (req, res) => {
       //   console.log(user[0].dataValues);
 
       if (restaurant && user) {
-        const reserve = await Reserve.create({
-          date,
-          time,
-          pax,
-          UserId: user[0].dataValues.id,
-          RestaurantId: restaurant[0].dataValues.id,
-        });
-        // console.log(reserve);
-        return res.status(200).send(reserve);
+        // console.log(restaurant[0].dataValues.personas_max);
+        if (restaurant[0].dataValues.personas_max >= pax) {
+          const reserve = await Reserve.create({
+            date,
+            time,
+            pax,
+            UserId: user[0].dataValues.id,
+            RestaurantId: restaurant[0].dataValues.id,
+          });
+          await Restaurant.update(
+            {
+              personas_max: restaurant[0].dataValues.personas_max - pax,
+            },
+            {
+              where: {
+                id: restaurant[0].dataValues.id,
+              },
+            }
+          );
+          // console.log(restaurant[0].dataValues);
+          return res.status(200).send(reserve);
+        } else {
+          return res.status(400).json({
+            message: `Solo nos quedan ${restaurant[0].dataValues.personas_max} lugares disponibles`,
+          });
+        }
       } else {
         return res
           .status(400)
-          .send({ message: "Usuario/Restaurant no existe" });
+          .json({ message: "Usuario/Restaurant no existe" });
       }
     } else {
-      return res.status(400).send({ message: "Faltan rellenar campos" });
+      return res.status(400).json({ message: "Faltan rellenar campos" });
     }
   } catch (e) {
     return res.status(404).send({ message: "Petición inválida" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const reserve = await Reserve.findOne({
+      where: {
+        id,
+      },
+    });
+
+    // console.log('Soy restaurant', restaurant);
+    if (reserve) {
+      const restaurant = await Restaurant.findOne({
+        where: {
+          id: reserve.RestaurantId,
+        },
+      });
+      // console.log(reserve);
+      // console.log(restaurant);
+      const updatedRestaurant = await restaurant.update(
+        {
+          personas_max:
+            restaurant.dataValues.personas_max + reserve.dataValues.pax,
+        },
+        {
+          where: {
+            id: restaurant.dataValues.id,
+          },
+        }
+      );
+      // console.log(updatedRestaurant);
+      await reserve.destroy();
+
+      return res.status(200).send(updatedRestaurant);
+    }
+    return res
+      .status(400)
+      .json({ message: "No se ha podido encontrar la reserva a eliminar" });
+  } catch (e) {
+    return res.status(404).json({ message: "Petición inválida" });
   }
 });
 
