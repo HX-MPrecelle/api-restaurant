e43c0f5948ca1039f7552bdb52f129bac5afd361
MPrecelle <tinchoprecelle@gmail.com>
2022-02-13 12:56:17 -0300

    Fix routes

--- src/routes/reviewsRoute.js
+++ src/routes/reviewsRoute.js
@@ -53,7 +53,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/restaurant/:id/all", async (req, res) => {
   //id de restaurant
   const { id } = req.params;
 
